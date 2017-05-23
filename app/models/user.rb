120d05258250815f3815edafbce9b3562bc6f2a2
neymarsabin <reddevil.sabin@gmail.com>
2017-05-23 22:34:36 +0545

    add simple user search system

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,16 @@ class User < ApplicationRecord
   acts_as_voter
 
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
-  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
 
   has_many :personal_messages, dependent: :destroy
+
+  def self.search(search)
+    if search
+      where(["email LIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end
   
 end
